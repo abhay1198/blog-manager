6e671c848b24c779e27d261d8feae0cec0965472
abhay1198 <abhayagarwal1198@gmail.com>
2022-09-26 16:11:54 +0530

    header and footer link

---
+++ Abhay/BlogManager/Block/HeaderLink.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package     Abhay/BlogManager
+ * @version     1.0.0
+ * @author      Abhay
+ * @copyright   Copyright © 2021. All Rights Reserved.
+ */
+
+namespace Abhay\BlogManager\Block;
+
+class HeaderLink extends \Magento\Framework\View\Element\Html\Link
+{
+    protected function _toHtml()
+    {
+        if (false != $this->getTemplate()) {
+            return parent::_toHtml();
+        }
+        return '<li><a ' . $this->getLinkAttributes() . ' >' . $this->escapeHtml($this->getLabel()) . '</a></li>';
+    }
+}
